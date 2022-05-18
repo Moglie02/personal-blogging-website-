400f88eb5cd58680d2ebf92d365f5f8673203568
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-18 18:35:54 +0300

    initial commit

--- app/main/views.py
+++ app/main/views.py
@@ -8,11 +8,11 @@ from flask_login import login_required,current_user
 from ..email import mail_message
 import secrets
 import os
-from PIL import Image
+#from PIL import Image
 
 @main.route('/')
 def index():
-    quotes = get_quotes()
+    quotes = get_quotes(
     page = request.args.get('page',1, type = int )
     blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page, per_page = 3)
     return render_template('index.html', quote = quotes,blogs=blogs)
