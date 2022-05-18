0c59f9b60e3f248425dcc264c388ecc6e050d82e
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-18 18:36:26 +0300

    initial commit

--- app/main/views.py
+++ app/main/views.py
@@ -12,7 +12,7 @@ import os
 
 @main.route('/')
 def index():
-    quotes = get_quotes(
+    quotes = get_quotes
     page = request.args.get('page',1, type = int )
     blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page, per_page = 3)
     return render_template('index.html', quote = quotes,blogs=blogs)
